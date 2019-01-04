14957f3c83228e93bb236dce9f29fc5f08fc2eb6
Matt Ruston <mruston@underarmour.com>
2019-01-04 13:39:54 -0600

    Updated project structure

---
+++ OpenTracing/OpenTracing.h
@@ -0,0 +1,19 @@
+//
+//  OpenTracing.h
+//  OpenTracing
+//
+//  Created by Matthew Ruston on 1/4/19.
+//  Copyright © 2019 Under Armour, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenTracing.
+FOUNDATION_EXPORT double OpenTracingVersionNumber;
+
+//! Project version string for OpenTracing.
+FOUNDATION_EXPORT const unsigned char OpenTracingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenTracing/PublicHeader.h>
+
+
