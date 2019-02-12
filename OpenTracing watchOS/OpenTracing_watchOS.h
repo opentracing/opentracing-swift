04534e418a1903dee9205e21bde583f00711b3d3
Matt Ruston <mruston@underarmour.com>
2019-02-12 16:52:54 -0600

    Added watchOS target

---
+++ OpenTracing watchOS/OpenTracing_watchOS.h
@@ -0,0 +1,19 @@
+//
+//  OpenTracing_watchOS.h
+//  OpenTracing watchOS
+//
+//  Created by Matthew Ruston on 2/12/19.
+//  Copyright © 2019 Under Armour, Inc. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for OpenTracing_watchOS.
+FOUNDATION_EXPORT double OpenTracing_watchOSVersionNumber;
+
+//! Project version string for OpenTracing_watchOS.
+FOUNDATION_EXPORT const unsigned char OpenTracing_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenTracing_watchOS/PublicHeader.h>
+
+
